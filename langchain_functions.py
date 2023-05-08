2bac9b17e072a585047527c176cfa4f9fed5e914
Ubuntu <ubuntu@ip-172-31-8-112.ec2.internal>
2023-05-08 23:43:18 +0000

    Langchain functions update

---
+++ langchain_functions.py
@@ -0,0 +1,27 @@
+import os
+from langchain.llms import OpenAI
+from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+def ask_with_search(prompt):
+    search = GoogleSerperAPIWrapper()
+    llm = OpenAI(temperature=0)
+    search = GoogleSerperAPIWrapper()
+    tools = [
+        Tool(
+            name="Intermediate Answer",
+            func=search.run,
+            description="useful for when you need to ask with search"
+        )
+    ]
+
+    self_ask_with_search = initialize_agent(tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True)
+    result = self_ask_with_search.run(prompt)
+    return result
+    
+ask_with_search("Who is Ruobing Lu?")
